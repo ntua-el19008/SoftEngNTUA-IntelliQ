df7e17c2f0aacdd686fe77bbcc5f8d5356542e0e
vanourogeros <el19008@mail.ntua.gr>
2023-01-14 19:50:22 +0200

    Added example POST request at index.js

--- api-backend/index.js
+++ api-backend/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+
+// Middlewares
+app.use(bodyParser.json());
 
 // Initialize port for node application to run
 
@@ -8,6 +12,13 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
 
+// Get request to root
 app.get('/', (req, res) => {
     res.send('Hello World!');
+})
+
+// Post request
+app.post('/checkParser', (req, res) => {
+    console.log("Using body-parser: ", req.body.value)
+    res.send({body: req.body})
 })
