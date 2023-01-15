f798507d8dfbc2c2364bee4b8cf527db1960e174
el19034 <el19034@mail.ntua.gr>
2023-01-15 13:13:50 +0200

    Update intelliq_api.js

--- api-backend/routes/intelliq_api.js
+++ api-backend/routes/intelliq_api.js
@@ -1,36 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.use("/admin", require('../admin/admin.js'));
+router.use("/admin", require('./admin/admin.js'));
+router.use("/doanswer/:questionnaireID/:questionID/:session/:optionID",require("./DoAnswer"))
+router.use("/questionnaire/:questionnaireID",require("./Questionnaire"))
+router.use("/question/:questionnaireID/:questionID", require("./Question"))
+router.use("/getsessionanswers/:questionnaireID/:session",require("./GetSessionAnswers"))
+router.use("/getquestionanswers/:questionnaireID/:questionID",require("./GetQuestionAnswers"))
 
-router
-    .route("/questionnaire/:questionnaireID")
-    .get((req,res) => {
-
-    })
-
-router  
-    .route("/question/:questionnaireID/:questionID")
-    .get((req,res) => {
-
-    })
-
-router
-    .route("/doanswer/:questionnaireID/:questionID/:session/:optionID")
-    .post((req,res) => {
-
-    })
-
-router 
-    .route("/getsessionanswers/:questionnaireID/:session")
-    .get((req,res) => {
-
-    })
-
-router
-    .route("/getquestionanswers/:questionnaireID/:questionID")
-    .get((req,res) => {
-
-    })
-
-    module.exports = router
+module.exports = router
