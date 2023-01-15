e6217122fe0c141111e052e47cc886aefbb49496
Panagiotis Xanthopoulos <el19084@mail.ntua.gr>
2023-01-16 01:30:44 +0200

    Questionnaire creation files (WIP)

---
+++ frontend/templates/qqflow.js
@@ -0,0 +1,100 @@
+var questionData = JSON.parse(sessionStorage.getItem('questionData'));
+
+var qqgenData = JSON.parse(sessionStorage.getItem('qqgenData'));
+
+console.log(questionData);
+console.log(qqgenData);
+
+function createQnextSelect(questionIndex) {
+    var qid_arr = [];
+
+    var select = document.createElement("select");
+    select.name = "nextqID";
+
+    var option = document.createElement("option");
+    option.value = "NULL";
+    option.innerHTML = "NULL";
+    select.appendChild(option);
+
+    for (var i = 0; i < questionData.length; i++) {
+        if (questionData[i].qID !== questionIndex && !qid_arr.includes(questionData[i].qID)) {
+            var option = document.createElement("option");
+            option.value = questionData[i].qID;
+            option.innerHTML = questionData[i].qID;
+            select.appendChild(option);
+            qid_arr.push(questionData[i].qID);
+        }
+    }
+    return select;
+}
+
+function displayQuestions() {
+    var qcount = 1;
+    var optcount = 1;
+
+    for (var i = 0; i < questionData.length; i++) {
+        if (questionData[i].qtext === undefined) {
+            //option
+            var option = questionData[i];
+            var optionDiv = document.createElement("div");
+            var optionTitle = document.createElement("p");
+            optionTitle.innerHTML = "Option " + optcount + ": " + option.optID + " - " + option.opttxt;
+            var qnextSelect = createQnextSelect(option.qID);
+            optionDiv.appendChild(optionTitle);
+            optionDiv.appendChild(qnextSelect);
+            document.getElementById("questionsContainer").appendChild(optionDiv);
+            optcount++;
+        }
+        else {
+            //question
+            var questionDiv = document.createElement("div");
+            var questionTitle = document.createElement("h1");
+            questionTitle.style = "font-size: 1.2em;";
+            questionTitle.innerHTML = "<br>Question " + (qcount) + "(" + questionData[i].qID + ")" + "<br />" + questionData[i].qtext;
+            questionDiv.appendChild(questionTitle);
+            document.getElementById("questionsContainer").appendChild(questionDiv);
+            qcount++;
+            optcount = 1;
+        }
+    }
+}
+
+function parseAll() {
+    qqgenData.questions = [];
+
+    question = null;
+    for (var i = 0; i < questionData.length; i++) {
+        if (questionData[i].qtext === undefined) {
+            //option
+            question.options = [];
+            question.options.push(questionData[i]);
+        }
+        else {
+            //question
+
+            qqgenData.questions.push(question);
+            question = questionData[i];
+        }
+    }
+    qqgenData.questions.push(question);
+    qqgenData.questions.shift();
+    console.log(qqgenData);
+}
+
+function submitQnext() {
+    let selects = document.getElementsByName("nextqID");
+    let answers = [];
+
+    for (let i = 0; i < selects.length; i++) {
+        answers.push(selects[i].options[selects[i].selectedIndex].value);
+    }
+
+    var cnt = 0;
+    for (var i = 0; i < questionData.length; i++) {
+        if (questionData[i].qtext === undefined) {
+            questionData[i].nextqID = answers[cnt];
+            cnt++;
+        }
+    }
+    parseAll();
+}
---
+++ frontend/templates/qqinit.js
@@ -0,0 +1,43 @@
+var keywordCount = 1;
+
+function addKeyword() {
+    keywordCount++;
+
+    var newKeyword = document.createElement("div");
+    newKeyword.innerHTML = `
+			<div id="keyword${keywordCount}">
+                <br /><input type="text" class="keyword" /><br />
+                <input type="button" value="Remove Keyword" onclick="removeKeyword(${keywordCount})" />
+			</div>
+			`;
+    var keywordContainer = document.getElementById("keywordContainer");
+    keywordContainer.insertBefore(newKeyword, keywordContainer.lastChild);
+}
+
+function removeKeyword(keywordIndex) {
+    var keywordDiv = document.getElementById(`keyword${keywordIndex}`);
+    keywordDiv.parentNode.removeChild(keywordDiv);
+
+    // adjust the questionCount
+    keywordCount--;
+}
+
+function parseQQdata() {
+    var questionnaireID = document.getElementById("qqid").value;
+    var questionnaireTitle = document.getElementById("qqtitle").value;
+    var keywords = [];
+    var keywordElements = document.getElementsByClassName("keyword");
+    for (var i = 0; i < keywordElements.length; i++) {
+        keywords.push(keywordElements[i].value);
+    }
+
+    var data = {
+        "questionnaireID": questionnaireID,
+        "questionnaireTitle": questionnaireTitle,
+        "keywords": keywords
+    };
+
+    var jsonData = JSON.stringify(data);
+    sessionStorage.setItem("qqgenData", jsonData);
+    window.location.href = "./qcreation.html";
+}
