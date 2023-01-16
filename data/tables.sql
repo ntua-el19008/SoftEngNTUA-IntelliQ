3fb1671cb1e574bb6db31f6f14463d624b1e2aa7
vanourogeros <el19008@mail.ntua.gr>
2023-01-16 23:20:51 +0200

    Update tables.sql

--- data/tables.sql
+++ data/tables.sql
@@ -11,38 +11,42 @@ Mask varchar(255)
 
 DROP TABLE IF EXISTS Question;
 CREATE TABLE Question (
-QID char(10) PRIMARY KEY,
+QID char(10) NOT NULL,
 QQID char(10) NOT NULL,
 Qtext varchar(255),
 Mandatory bit,
 Personal bit,
-CONSTRAINT QuestionQuestionnaire FOREIGN KEY (QQID) REFERENCES Questionnaire (QQID)
+CONSTRAINT QuestionQuestionnaire FOREIGN KEY (QQID) REFERENCES Questionnaire (QQID),
+PRIMARY KEY (QID, QQID)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS Keyword ;
 CREATE TABLE Keyword (
 QQID char(10) NOT NULL,
-Keyword varchar(255) PRIMARY KEY,
-CONSTRAINT KeywordQuestionnaire FOREIGN KEY (QQID) REFERENCES Questionnaire (QQID)
+Keyword varchar(255) NOT NULL,
+CONSTRAINT KeywordQuestionnaire FOREIGN KEY (QQID) REFERENCES Questionnaire (QQID),
+PRIMARY KEY (QQID, Keyword)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS Choice ;
 CREATE TABLE Choice (
-ChoiceID char(10) PRIMARY KEY,
+ChoiceID char(10) NOT NULL,
 QQID char(10) NOT NULL,
 QID char(10) NOT NULL,
 ChoiceText varchar(255) NOT NULL,
 NextQID char(10) NOT NULL,
 CONSTRAINT ChoicesQuestionnaire FOREIGN KEY (QQID) REFERENCES Questionnaire(QQID) ,
-CONSTRAINT ChoicesQuestion FOREIGN KEY (QID) REFERENCES Question(QID)
+CONSTRAINT ChoicesQuestion FOREIGN KEY (QID) REFERENCES Question(QID),
+PRIMARY KEY (ChoiceID, QQID, QID)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 DROP TABLE IF EXISTS Participant ;
 CREATE TABLE Participant (
-SessionID char(10) PRIMARY KEY,
+SessionID char(10) NOT NULL,
 QQID char(10) NOT NULL,
-CONSTRAINT ParticipantsQuestionnaire FOREIGN KEY (QQID) REFERENCES Questionnaire(QQID) 
+CONSTRAINT ParticipantsQuestionnaire FOREIGN KEY (QQID) REFERENCES Questionnaire(QQID),
+PRIMARY KEY (SessionID, QQID)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS Answer ;
@@ -54,11 +58,6 @@ ChoiceID char(10) Not null,
 CONSTRAINT AnswersQuestionnaire FOREIGN KEY (QQID) REFERENCES Questionnaire (QQID),
 CONSTRAINT AnswersQuestion FOREIGN KEY (QID) REFERENCES Question(QID),
 CONSTRAINT AnswersChoices FOREIGN KEY (ChoiceID) REFERENCES Choice (ChoiceID),
-CONSTRAINT AnswersSession FOREIGN KEY (SessionID) REFERENCES Participant (SessionID)
+CONSTRAINT AnswersSession FOREIGN KEY (SessionID) REFERENCES Participant (SessionID),
+PRIMARY KEY (QQID, QID, SessionID)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-
-
-
-
