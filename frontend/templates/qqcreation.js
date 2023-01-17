a1b7e29fd160c6eb1ac76c8d34ea0b8f281366f6
ntua-el19084 <panos0511@gmail.com>
2023-01-17 20:22:21 +0200

    Bug fixes and appearance

--- frontend/templates/qqcreation.js
+++ frontend/templates/qqcreation.js
@@ -7,8 +7,10 @@ function addQuestion() {
 
     var newQuestion = document.createElement("div");
     newQuestion.innerHTML = `
-        <div id="question${questionCount}">
-            <h1 style="font-size: 1.5em;">Question ${questionCount}</h1>
+        <br><div id="question${questionCount}" style="text-align: center;">
+            <h1 style="font-size: 1.7em;">Question ${questionCount}</h1>
+            <input type="button" value="Remove Question" class="btn btn-danger" onclick="removeQuestion(${questionCount})"
+			style="margin: 0 auto; display: block;" /><br>
             <label style="font-size: 1.2em;">Question ID</label><br />
             <input type="text" name="qid" /><br />
             <label style="font-size: 1.2em;">Question text</label><br />
@@ -31,8 +33,8 @@ function addQuestion() {
                 <input type="radio" id="question" name="type" value="question">
                 <br><br>
             </form>
-            <input type="button" value="Remove Question" onclick="removeQuestion(${questionCount})" /><br /><br />
-            <input type="button" value="+" onclick="addOption(${questionCount})" /><br />
+            <input type="button" value="+" class="btn btn-danger" onclick="addOption(${questionCount})"
+			style="margin: 0 auto; display: block;" /><br>
             <div id="optionsContainer${questionCount}">
             <div id="option1">
                 <label style="font-size: 1.2em;">Option ID</label><br />
@@ -53,7 +55,7 @@ function removeQuestion(questionIndex) {
 
     // adjust the questionCount and optionCount
     questionCount--;
-    optionCount.splice(questionIndex, 1);
+    delete optionCount[questionIndex];
 }
 
 function addOption(questionNum) {
@@ -65,7 +67,8 @@ function addOption(questionNum) {
             <input type="text" name="opt" /><br />
             <label style="font-size: 1.2em;">Option Text</label><br />
             <input type="text" name="opttxt" /><br />
-            <input type="button" value="Remove Option" onclick="removeOption(this, ${questionNum})" /><br />
+            <input type="button" value="Remove Option" class="btn btn-danger" onclick="removeOption(this, ${questionNum})"
+			style="margin: 0 auto; display: block;" /><br>
         </div>
         `;
     var optionsContainer = document.getElementById("optionsContainer" + questionNum);
@@ -87,7 +90,8 @@ function parseQuestions() {
         if (questions[i].id.startsWith("question")) {
             var typeButtons = document.getElementsByName("type");
             var selectedValuetype;
-            for (var j = 0; i < typeButtons.length; j++) {
+
+            for (var j = 0; j < typeButtons.length; j++) {
                 if (typeButtons[j].checked) {
                     selectedValuetype = typeButtons[j].value;
                     break;
@@ -96,7 +100,7 @@ function parseQuestions() {
 
             var mandatoryButtons = document.getElementsByName("mandatory");
             var selectedValuemand;
-            for (var j = 0; i < mandatoryButtons.length; j++) {
+            for (var j = 0; j < mandatoryButtons.length; j++) {
                 if (mandatoryButtons[j].checked) {
                     selectedValuemand = mandatoryButtons[j].value;
                     break;
@@ -112,8 +116,13 @@ function parseQuestions() {
                 }
                 if (inputs[j].name === "qtext") {
                     if (!questionAdded) {
-                        questionData.push({ "qID": qid, "qtext": inputs[j].value, "required": selectedValuemand, "type": selectedValuetype });
-                        questionAdded = true;
+                        if (qid !== "" && inputs[j].value !== "" && selectedValuemand !== undefined && selectedValuetype !== undefined) {
+                            questionData.push({ "qID": qid, "qtext": inputs[j].value, "required": selectedValuemand, "type": selectedValuetype });
+                            questionAdded = true;
+                        } else {
+                            document.getElementById("error-box").style.display = "flex";
+                            return;
+                        }
                     }
                 }
             }
@@ -127,9 +136,17 @@ function parseQuestions() {
                     for (var l = 0; l < optionInputs.length; l++) {
                         if (optionInputs[l].name === "opt") {
                             opt = optionInputs[l].value;
+                            if (opt === "") {
+                                document.getElementById("error-box").style.display = "flex";
+                                return;
+                            }
                         }
                         if (optionInputs[l].name === "opttxt") {
                             opttxt = optionInputs[l].value;
+                            if (opttxt === "") {
+                                document.getElementById("error-box").style.display = "flex";
+                                return;
+                            }
                         }
                         if (opt !== "" && opttxt !== "") {
                             var option = opt + opttxt;
