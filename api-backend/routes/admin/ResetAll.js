5cbefcc9fc348fa7b53409c725c6c66f9dd0ba54
adamkapetis <adam25501@hotmail.com>
2023-01-17 12:53:28 +0200

    ResetAll,ResetQ

--- api-backend/routes/admin/ResetAll.js
+++ api-backend/routes/admin/ResetAll.js
@@ -4,6 +4,23 @@ const router = express.Router();
 router
     .route("/resetall")
     .post((req,res) => {
+        var query = 'delete * from Participant';
+        pool.getConnection((err,conn)=> {
+            if(err){
+                res.json({status:"failed", reason:"failed to connect to db"})
+                return;
+            }
+            conn.query(query,(err,rows,fields)=>{
+                if(err){
+                    res.json({status:"failed",reason:err.toString()})
+                    conn.release();
+                    return;
+                }
+                res.json({status:"OK"})
+                conn.release();
+            })
+
+        })
 
     })
 
--- api-backend/routes/admin/ResetQ.js
+++ api-backend/routes/admin/ResetQ.js
@@ -4,6 +4,24 @@ const router = express.Router();
 router
     .route("/resetq/:questionnaireID")
     .post((req,res) => {
+        const qqid = req.params.questionnaireID;
+        const query = `delete * from Participant WHERE QQID = '${qqid}';`;
+        pool.getConnection((err,conn)=> {
+            if(err){
+                res.json({status:"failed", reason:"failed to connect to db"})
+                return;
+            }
+            conn.query(query,(err,rows,fields)=>{
+                if(err){
+                    res.json({status:"failed",reason:err.toString()})
+                    conn.release();
+                    return;
+                }
+                res.json({status:"OK"})
+                conn.release();
+            })
+
+        })
 
     })
 
