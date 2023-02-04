ca7ff9edfcaa27543cb362054687fe2ed48945d3
Myrsini Kellari <115076807+ntua-el19082@users.noreply.github.com>
2023-02-05 01:46:21 +0200

    successfully fetching questions and options

--- frontend/templates/qqanswer.js
+++ frontend/templates/qqanswer.js
@@ -1,10 +1,74 @@
 var questionnaireData = JSON.parse(sessionStorage.getItem('questionnaireData'));
+var qindex = 0;
+var question_result = {};
+
 function get() {
-    //alert(questionnaireData["questionnaireTitle"]);
-    // sessionStorage.clear();
-  }
+    var question = questionnaireData["questions"][qindex];
+    var showq = document.createElement("div");
+    showq.innerHTML = `
+    <div>
+    <form id="questionnaireIntro" style="text-align: center; padding: 10px 20px;">
+    <h2>${question["qtext"]}
+    <h2/>
+    </form>
+    </div>
+    `;
+    var questionContainer = document.getElementById("card");
+    questionContainer.replaceChildren(showq);
+    const questionID =  question["qid"];
+    const questionnaireID = questionnaireData["questionnaireID"];
+    fetch(`/intelliq_api/question/${questionnaireID}/${questionID}`)
+    .then(response => response.json())
+    // .then(response => console.log(response))
+    .then(data => {
+        if (data.hasOwnProperty("error")) {
+        }
+        else {
+            question_result = {
+                "questionnaireID": data["questionnaireID"],
+                "qID": data["qID"],
+                "qtext": data["qtext"],
+                "required": data["required"],
+                "type": data["type"],
+                "options": data["options"]
+            }
+
+            for (var i = 0; i < question_result["options"].length; i++) {
+                //alert(question["qtext"]);
+                var option = document.createElement("div");
+                option.innerHTML = `
+                
+                <div class="table-responsive" style="text-align: center; "> 
+                   
+                        <div class="form-check" >
+                            <input class="form-check-input"  type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+                            <label class="form-check-label" for="exampleRadios1">
+                            ${ question_result["options"][i]["opttxt"]}
+                            </label>
+                        </div>
+                    
+                </div>
+                `;
+                questionContainer.appendChild(option);}
+        }
+    })
+    .catch(error => console.error(error));
+    qindex += 1;
+    
+    }
 
 function display() {
-
-    alert(questionnaireData["questionnaireTitle"]);
+    var title = questionnaireData["questionnaireTitle"];
+    var question = document.createElement("div");
+    question.innerHTML = `
+    <div>
+    <form id="questionnaireIntro" style="text-align: center; padding: 10px 20px;">
+    <h1>${title}
+    <h1/>
+    </form>
+    </div>
+    `;
+    var questionContainer = document.getElementById("header");
+    questionContainer.appendChild(question);
+   // alert(questionnaireData["questionnaireTitle"]);
 }
--- frontend/templates/qqselect.js
+++ frontend/templates/qqselect.js
@@ -1,10 +1,6 @@
 const firstButton = document.getElementById("firstButton");
-    const secondButton = document.getElementById("secondButton");
-    firstButton.addEventListener("click", function() {
-        secondButton.style.display = "block";
-      });
+const secondButton = document.getElementById("secondButton");
 
-var QID;
 var questionnaire_result = {};
 
 function fetchQuestionnaire(){
@@ -14,12 +10,16 @@ function fetchQuestionnaire(){
         .then(response => response.json())
        // .then(response => console.log(response))
         .then(data => {
-            
-            
+            var questionContainer = document.getElementById("card");
             if (data.hasOwnProperty("error")) {
                alert("The Questionnaire is empty, or doesn't exist. Please give a valid Questionnaire ID.");
+               secondButton.style.display = "none";
+               var question = document.createElement("div");
+                question.innerHTML = `<div></div>`;
+               questionContainer.replaceChildren(question);
             }
             else {
+                secondButton.style.display = "block";
                 var title = data["questionnaireTitle"];
                 var question = document.createElement("div");
                 question.innerHTML = `
@@ -32,10 +32,8 @@ function fetchQuestionnaire(){
                 </form>
                 </div>
                 `;
-                var questionContainer = document.getElementById("card");
                 //questionContainer.appendChild(question);
                 questionContainer.replaceChildren(question);
-                QID = questionnaireID;
                 questionnaire_result = {
                     "questionnaireID": questionnaireID,
                     "questionnaireTitle": title,
@@ -48,27 +46,9 @@ function fetchQuestionnaire(){
 
 }
 
+//change page and pass questionnaire data
 function answer() {
-    //const questionnaireID = document.getElementById("questionnaireID").value;
-    var question = document.createElement("div");
-    question.innerHTML = `
-        <div>
-        <form id="questionnaireIntro" style="text-align: center; padding: 10px 20px;">
-        <h1>Give answers for questionnaire with title :<h1/>
-        <h2>${QID}<h2/>
-        <h3>Are you sure you want to answer this questionnaire?
-        <h3/>
-        </form>
-        </div>
-        `;
-    var questionContainer = document.getElementById("card");
-    //questionContainer.appendChild(question);
-    //var json = JSON.stringify(questionnaire_result);
-    //sessionStorage.setItem("questionnaire_result", json);
-    alert(questionnaire_result["questionnaireTitle"]);
     var json = JSON.stringify(questionnaire_result);
     sessionStorage.setItem("questionnaireData", json);
     window.location.href = "/answer";
 }
-
-
