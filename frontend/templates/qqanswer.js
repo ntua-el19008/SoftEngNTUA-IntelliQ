22da5574f08424476233089b1b44eb5a248c2028
Myrsini Kellari <115076807+ntua-el19082@users.noreply.github.com>
2023-02-11 18:52:36 +0200

    answering questionnaires now fullt functioning

--- frontend/templates/qqanswer.js
+++ frontend/templates/qqanswer.js
@@ -1,7 +1,27 @@
 var questionnaireData = JSON.parse(sessionStorage.getItem('questionnaireData'));
 var qindex = 0;
 var question_result = {};
+var answers = [];
+var session;
 
+//fetching first question when page loads
+function display() {
+    
+    var title = questionnaireData["questionnaireTitle"];
+    var question = document.createElement("div");
+    question.innerHTML = `
+    <div>
+    <form id="questionnaireIntro" style="text-align: center; padding: 10px 20px;">
+    <h1>${title}
+    <h1/>
+    </form>
+    </div>
+    `;
+    var questionContainer = document.getElementById("header");
+    questionContainer.appendChild(question);
+}
+
+//this function pulls the options for every question and displays them
 function get() {
     var question = questionnaireData["questions"][qindex];
     var showq = document.createElement("div");
@@ -19,7 +39,6 @@ function get() {
     const questionnaireID = questionnaireData["questionnaireID"];
     fetch(`/intelliq_api/question/${questionnaireID}/${questionID}`)
     .then(response => response.json())
-    // .then(response => console.log(response))
     .then(data => {
         if (data.hasOwnProperty("error")) {
         }
@@ -34,41 +53,92 @@ function get() {
             }
 
             for (var i = 0; i < question_result["options"].length; i++) {
-                //alert(question["qtext"]);
                 var option = document.createElement("div");
                 option.innerHTML = `
                 
-                <div class="table-responsive" style="text-align: center; "> 
+                <div class="table-responsive" > 
                    
                         <div class="form-check" >
-                            <input class="form-check-input"  type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+                            <input class="form-check-input"  type="radio" name="exampleRadios" id="exampleRadios1" value="option1">
                             <label class="form-check-label" for="exampleRadios1">
                             ${ question_result["options"][i]["opttxt"]}
                             </label>
-                        </div>
-                    
+                        </div> 
                 </div>
                 `;
-                questionContainer.appendChild(option);}
-        }
+                questionContainer.appendChild(option);
+            }
+            var butt = document.createElement("div");
+            butt.innerHTML = `
+                <div>
+                <button id="secondButton" class="btn btn-danger"  onclick=" fetchNext(question_result)"
+                        style="margin: 0 auto; display: block;">Next</button>
+                </div>`;
+            questionContainer.appendChild(butt);
+        }    
     })
     .catch(error => console.error(error));
-    qindex += 1;
-    
+}
+
+function fetchNext(question_result) {
+    //take the chosen option and append it to answers
+    var optindex = 0;
+    var ele = document.getElementsByName('exampleRadios'); 
+    for(i = 0; i < ele.length; i++) {
+        if (ele[i].checked) {
+            optindex = i;
+        }
     }
 
-function display() {
-    var title = questionnaireData["questionnaireTitle"];
-    var question = document.createElement("div");
-    question.innerHTML = `
-    <div>
-    <form id="questionnaireIntro" style="text-align: center; padding: 10px 20px;">
-    <h1>${title}
-    <h1/>
-    </form>
-    </div>
-    `;
-    var questionContainer = document.getElementById("header");
-    questionContainer.appendChild(question);
-   // alert(questionnaireData["questionnaireTitle"]);
+    var temp = {
+        "questionnaireID": question_result["questionnaireID"],
+        "qID": question_result["qID"],
+        "qtext": question_result["qtext"],
+        "type": question_result["type"],
+        "optID": question_result["options"][optindex]["optID"],
+        "optiontxt": question_result["options"][optindex]["opttxt"],
+        "nextopt": question_result["options"][optindex]["nextqID"]
+    };
+
+    answers.push(temp);
+
+    if (qindex == (questionnaireData["questions"].length - 1)) {
+       
+        var butt2 = document.getElementById("secondButton");
+        butt2.style.display = "none"; 
+        //tha apantaw epitelous
+
+
+        var butt = document.getElementById("submitButton");
+        butt.style.display = "block"; 
+        
+    }
+    else {
+        if (temp["nextopt"] == NULLQ) {
+            qindex += 1;
+        }
+        else {
+            for (var i = 0; i < questionnaireData.length; i++) {
+                if  (questionnaireData["questions"][i]["qid"] == temp["nextopt"]) {
+                    qindex = i;
+                    break;
+                }
+            }
+        }
+        get();
+    }
+}
+
+
+
+//this function actually submits the aswers
+function submit() {
+    const questionnaireID = questionnaireData["questionnaireID"];
+    for (var i = 0; i < answers.length; i++) {
+        var qid = answers[i]["qID"];
+        var opt = answers[i]["optID"];
+        fetch(`/intelliq_api/doanswer/${questionnaireID}/${qid}/${session}/${opt}`, {method: 'POST'})
+        .then(response => response.json())
+        .catch(error => console.error(error));
+    }
 }
